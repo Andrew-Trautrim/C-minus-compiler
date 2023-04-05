b096e3f214a6a39cc3d0015ff47ee834e54ef74c
Andrew Trautrim <atrautrim7@gmail.com>
2023-04-05 00:40:53 -0400

    Declarations

--- CodeGenerator.java
+++ CodeGenerator.java
@@ -16,7 +16,7 @@ public class CodeGenerator implements AbsynVisitor<Void> {
 
     private String filename;
     private FileWriter code;
-    private int mainEntry, globalOffset;
+    private int mainEntry, frameOffset, globalOffset;
     private static int emitLoc, highEmitLoc;
 
     /* Constructor */
@@ -60,7 +60,7 @@ public class CodeGenerator implements AbsynVisitor<Void> {
         emit(String.format("%3d: %5s %d, %d(%d)", emitLoc, op, r, a - (emitLoc + 1), pc));
         emit(String.format("\t%s\n", c));
 
-        ++emitLoc;
+        emitLoc++;
         if (highEmitLoc < emitLoc) {
             highEmitLoc = emitLoc;
         }
@@ -149,9 +149,6 @@ public class CodeGenerator implements AbsynVisitor<Void> {
         // generate prelude
         generatePrelude();
 
-        // temporary main function
-        //generateMain();
-
         // make a request to the visit method for DecList
         trees.accept(this, 0, false);
 
@@ -159,11 +156,24 @@ public class CodeGenerator implements AbsynVisitor<Void> {
         generateFinale();
     }
  
-    public Void visit(SimpleVar exp, int r, boolean isAddr) { 
+    public Void visit(SimpleVar exp, int r, boolean isAddr) {
+        emitRM("LD", r, exp.dec.offset, exp.dec.nestLevel == 0 ? gp : fp, "load variable " + exp.name + " into reg " + r);
         return null; 
     }
-    public Void visit(IndexVar exp, int r, boolean isAddr) { 
 
+    public Void visit(IndexVar exp, int r, boolean isAddr) {
+        exp.index.accept(this, r + 1, false); // load index offset to register r + 1
+        if (exp.dec.nestLevel == 0) { // global scope
+            emitRO("ADD", r + 1, r + 1, gp, "add gp to reg " + (r + 1));
+        }
+        else { // local scope
+            emitRO("ADD", r + 1, r + 1, fp, "add fp to reg " + (r + 1));
+        }
+        // add one to offset size storage
+        emitRM("LDC", r, 1, 0, "load 1 into reg " + r);
+        emitRO("ADD", r + 1, r + 1, r, "add 1 to reg " + (r + 1));
+
+        emitRM("LD", r, exp.dec.offset, r + 1, "load variable " + exp.name + " into reg " + r); 
         return null; 
     }
 
@@ -172,37 +182,43 @@ public class CodeGenerator implements AbsynVisitor<Void> {
         return null; 
     }
 
-    public Void visit(BoolExp exp, int r, boolean isAddr) { 
+    public Void visit(BoolExp exp, int r, boolean isAddr) {
         emitRM("LDC", r, exp.value ? 1 : 0, 0, "load " + exp.value + " into reg " + r);
         return null; 
     }
 
-    public Void visit(VarExp exp, int value, boolean isAddr) { 
-        exp.variable.accept(this, 0, true);
+    public Void visit(VarExp exp, int r, boolean isAddr) {
+        exp.variable.accept(this, r, true);
         return null; 
     }
 
-    public Void visit(CallExp exp, int value, boolean isAddr) { return null; }
+    public Void visit(CallExp exp, int value, boolean isAddr) {
+        emitComment("-> call statement: " + exp.func);
+        
+        emitComment("<- call statement: " + exp.func);
+
+        return null;
+    }
 
     public Void visit(OpExp exp, int r, boolean isAddr) {
     
         exp.left.accept(this, r, false);
         exp.right.accept(this, r + 1, false);
         if (exp.op == OpExp.PLUS) {
-            emitRO("ADD", r, r, r + 1, "");
+            emitRO("ADD", r, r, r + 1, "add reg " + r + " to reg " + (r + 1));
         }
         else if (exp.op == OpExp.MINUS) {
-            emitRO("SUB", r, r, r + 1, "");
+            emitRO("SUB", r, r, r + 1, "subtract reg " + (r + 1) + " from reg " + r);
         }
         else if (exp.op == OpExp.UMINUS) {
-            emitRM("LDC", r, 0, 0, "");
-            emitRO("SUB", r, r, r + 1, "");
+            emitRM("LDC", r, 0, 0, "zero register " + r);
+            emitRO("SUB", r, r, r + 1, "subtract reg " + (r + 1) + " from reg " + r);
         }
         else if (exp.op == OpExp.TIMES) {
-            emitRO("MUL", r, r, r + 1, "");
+            emitRO("MUL", r, r, r + 1, "multiply reg " + r + " by reg " + (r + 1));
         }
         else if (exp.op == OpExp.DIVIDE) {
-            emitRO("DIV", r, r, r + 1, "");
+            emitRO("DIV", r, r, r + 1, "divide reg " + r + " by reg " + (r + 1));
         }
         else if (exp.op == OpExp.EQ || exp.op == OpExp.NEQ || exp.op == OpExp.LT || exp.op == OpExp.LEQ || exp.op == OpExp.GT || exp.op == OpExp.GEQ) {
             emitRO("SUB", r, r, r + 1, "");
@@ -221,31 +237,119 @@ public class CodeGenerator implements AbsynVisitor<Void> {
     }
 
     public Void visit(AssignExp exp, int value, boolean isAddr) {
+        String name = "";
         exp.rhs.accept(this, ac, false); // compute righthand side and store the result in ac
 
         if (exp.lhs.variable instanceof SimpleVar) {
-            emitRM("LDC", ac1, 0, 0, "");
+            emitRM("ST", ac, exp.lhs.variable.dec.offset, exp.lhs.variable.dec.nestLevel == 0 ? gp : fp, "write reg " + ac + " to variable " + ((SimpleVar)exp.lhs.variable).name);
         }
         else if (exp.lhs.variable instanceof IndexVar) {
-            ((IndexVar)exp.lhs.variable).index.accept(this, ac1, false); // save index to ac1 register
+            ((IndexVar)exp.lhs.variable).index.accept(this, ac1, false); // save index to ac1 register -> can we have an assign expression in the index?
+            if (exp.lhs.variable.dec.nestLevel == 0) { // global scope
+                emitRO("ADD", ac1, ac1, gp, "add gp to reg " + ac1);
+            }
+            else { // local scope
+                emitRO("ADD", ac1, ac1, fp, "add fp to reg " + ac1);
+            }
+            // add one to offset size storage
+            emitRM("LDC", ac1 + 1, 1, 0, "load 1 into reg " + (ac1 + 1));
+            emitRO("ADD", ac1, ac1, ac1 + 1, "add 1 to reg " + ac1);
+
+            emitRM("ST", ac, exp.lhs.variable.dec.offset, ac1, "write reg " + ac + " to variable " + ((IndexVar)exp.lhs.variable).name);
         }
-        emitRM("ST", ac, variableLoc(exp.lhs.variable), ac1, ""); // save result to left hand side variable
 
         return null; 
     }
 
-    public Void visit(IfExp exp, int value, boolean isAddr) { return null; }
-    public Void visit(WhileExp exp, int value, boolean isAddr) { return null; }
-    public Void visit(ReturnExp exp, int value, boolean isAddr) { return null; }
-    public Void visit(CompoundExp exp, int value, boolean isAddr) { return null; }
+    public Void visit(IfExp exp, int value, boolean isAddr) {
+        emitComment("-> if statement");
+
+        // if test
+
+        // conditional jump
+
+        // code for TRUE case
+        
+        // unconditional jump
+
+        // code for FALSE case
+
+        emitComment("<- if statement");
+        return null; 
+    }
+
+    public Void visit(WhileExp exp, int value, boolean isAddr) {
+        emitComment("-> while statement");
+        
+        emitComment("<- while statement");
+        return null; 
+    }
+
+    public Void visit(ReturnExp exp, int value, boolean isAddr) { 
+        emitRM("LD", pc, retFO, fp, "return to caller");
+        return null; 
+    }
+
+    public Void visit(CompoundExp exp, int value, boolean isAddr) { 
+        emitComment("-> compound statement");
+        exp.decs.accept(this, 0, false);
+        exp.exps.accept(this, 0, false);
+        emitComment("<- compound statement");
+
+        return null; 
+    }
  
-    public Void visit(FunctionDec exp, int value, boolean isAddr) { return null; }
+    public Void visit(FunctionDec exp, int value, boolean isAddr) { 
+        exp.funcAddr = emitLoc;
+        emitComment("-> function: " + exp.func);
+        emitRM("ST", 0, retFO, fp, "save return PC");
+        frameOffset = initFO;
+
+        exp.params.accept(this, 0, false);
+        exp.body.accept(this, 0, false);
+        
+        emitComment("<- function: " + exp.func);
+
+        return null;
+    }
  
-    public Void visit(SimpleDec exp, int value, boolean isAddr) { 
+    public Void visit(SimpleDec exp, int value, boolean isAddr) {
+        if (exp.nestLevel == 0) { // global scope
+            emitComment("allocating global variable " + exp.name + " at offset " + globalOffset);
+            exp.offset = globalOffset;
+            globalOffset--;
+        }
+        else { // local scope
+            emitComment("allocating local variable " + exp.name + " at offset " + frameOffset);
+            exp.offset = frameOffset;
+            frameOffset--;
+        }
+
         return null; 
     }
 
     public Void visit(ArrayDec exp, int value, boolean isAddr) { 
+        if (exp.nestLevel == 0) { // global scope
+            globalOffset -= exp.size;
+            emitComment("allocating global variable " + exp.name + "[" + exp.size + "]" + " at offset " + globalOffset);
+            exp.offset = globalOffset;
+
+            emitRM("LDC", ac, exp.size, 0, "load array size");
+            emitRM("ST", ac, globalOffset, gp, "");
+            
+            globalOffset--;
+        }
+        else { // local scope
+            frameOffset -= exp.size;
+            emitComment("allocating local variable " + exp.name + "[" + exp.size + "]" + " at offset " + frameOffset);
+            exp.offset = frameOffset;
+            
+            emitRM("LDC", ac, exp.size, 0, "load array size");
+            emitRM("ST", ac, frameOffset, fp, "");
+            
+            frameOffset--;
+        }
+
         return null; 
     }
 
@@ -281,10 +385,10 @@ public class CodeGenerator implements AbsynVisitor<Void> {
 
     /* Wont be visited */
     public Void visit(NameTy exp, int value, boolean isAddr) { return null; }
-    public Void visit(NilExp exp, int r, boolean isAddr) { return null; }
+    public Void visit(NilExp exp, int value, boolean isAddr) { return null; }
 
     /* Get location of variable */
     private int variableLoc(Var var) {
-        return ((var.dec.nestLevel == 0) ? gp + globalOffset : fp) + var.dec.offset;
+        return ((var.dec.nestLevel == 0) ? gp : fp) + var.dec.offset;
     }
 }
--- test.tm
+++ test.tm
@@ -16,10 +16,34 @@
  10:    LD 7, -1(5)	return to caller
   3:   LDA 7, 7(7)	
 * End of standard prelude.
+* allocating global variable x[2] at offset -2
+ 11:   LDC 0, 2(0)	load array size
+ 12:    ST 0, -2(6)	
+* allocating global variable y at offset -3
+* -> function: main
+ 13:    ST 0, -1(5)	save return PC
+* -> compound statement
+* allocating local variable a[2] at offset -4
+ 14:   LDC 0, 2(0)	load array size
+ 15:    ST 0, -4(5)	
+ 16:   LDC 0, 5(0)	load 5 into reg 0
+ 17:   LDC 2, 0(0)	load 0 into reg 2
+ 18:   ADD 2, 2, 5	add fp to reg 2
+ 19:   LDC 1, 1(0)	load 1 into reg 1
+ 20:   ADD 2, 2, 1	add 1 to reg 2
+ 21:    LD 1, -4(2)	load variable a into reg 1
+ 22:   ADD 0, 0, 1	add reg 0 to reg 1
+ 23:   LDC 1, 1(0)	load 1 into reg 1
+ 24:   ADD 1, 1, 6	add gp to reg 1
+ 25:   LDC 2, 1(0)	load 1 into reg 2
+ 26:   ADD 1, 1, 2	add 1 to reg 1
+ 27:    ST 0, -2(1)	write reg 0 to variable x
+* <- compound statement
+* <- function: main
 * Finale:
- 11:    ST 5, 0(5)	push ofp
- 12:   LDA 5, 0(5)	push frame
- 13:   LDA 0, 1(7)	load ac with ret ptr
- 14:   LDA 7, -15(7)	jump to main loc
- 15:    LD 5, 0(5)	pop frame
- 16:  HALT 0, 0, 0	
+ 28:    ST 5, -4(5)	push ofp
+ 29:   LDA 5, -4(5)	push frame
+ 30:   LDA 0, 1(7)	load ac with ret ptr
+ 31:   LDA 7, -32(7)	jump to main loc
+ 32:    LD 5, 0(5)	pop frame
+ 33:  HALT 0, 0, 0	
